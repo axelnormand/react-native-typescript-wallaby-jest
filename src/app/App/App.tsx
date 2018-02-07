2e8adad99126c3c540a303f1d8384ca17ae4beae
axelnormand <imoffskating@gmail.com>
2018-02-07 22:12:58 +0000

    centre view

--- src/app/App/App.tsx
+++ src/app/App/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const App = () => (
-  <Text>Hello World!</Text>
+  <View style={styles.container}>
+    <Text>Hello World!</Text>
+  </View>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
